19b803b05724103f249f826c0ab2c64e8f0c7b54
Egidio Caprino <egidio.caprino@gmail.com>
2023-01-18 09:39:58 +0100

    test: set up 80% minimum code coverage

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,28 @@
-const nextJest = require('next/jest')
+const nextJest = require('next/jest');
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
+});
 
-// Add any custom config to be passed to Jest
-/** @type {import('jest').Config} */
+const minCoveragePercentage = 80;
+
 const customJestConfig = {
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
-}
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/pages/**',
+    '!<rootDir>/pages/_app.tsx',
+    '!<rootDir>/pages/_document.tsx',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: minCoveragePercentage,
+      functions: minCoveragePercentage,
+      lines: minCoveragePercentage,
+      statements: minCoveragePercentage,
+    },
+  },
+};
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig(customJestConfig)
+module.exports = createJestConfig(customJestConfig);
