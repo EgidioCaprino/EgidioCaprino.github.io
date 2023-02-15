6e28221a30415938858969ca5b46c10268117223
Egidio Caprino <egidio.caprino@gmail.com>
2023-02-15 11:49:06 +0100

    feat: add home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/globals.css';
+import 'node_modules/highlight.js/styles/solarized-dark.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
