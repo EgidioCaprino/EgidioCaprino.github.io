8509d37e6222fc85a41eb5ec2ac8ed3a658af9c2
EgidioCaprino <EgidioCaprino@users.noreply.github.com>
2023-02-15 14:36:33 +0000

    deploy: 74a3af156509a28fbfd88b3fe822585fd5dc2aa0

---
+++ _next/static/Y-JpPGGUbxuau8rdPwRI1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/376-af80e110a7d4298d.js","static/chunks/pages/index-bbafb989db0bf594.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- about.markdown
+++
@@ -1,8 +0,0 @@
----
-layout: page
-title: About
-permalink: /about/
----
-Hi there 👋
-
-My name is Egidio Caprino and I'm a software engineer. This space is for some personal thoughts and notes. Please, reach out if you have any questions or feedback. My email address is down there 👇
